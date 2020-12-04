0c9566a04a7b9bd1888ca74657c5ba36f259caf6
Anton.io <tony_steel@mail.ru>
2020-12-04 22:03:04 +0300

    Add FSA methods

--- MemoryManagement/MemoryAllocation.cpp
+++ MemoryManagement/MemoryAllocation.cpp
@@ -60,33 +60,53 @@ public:
 		// если есть head, тоесть есть свободный блок, то возвращаем ссылку на него
 		// если его нет в текущей странице, то создаем новую страницу
 		// если следующая страница уже создана, то переходим в нее и ищем там
-
-		auto currentFreeBlock = (char*)currentPage->headFL + sizeof(int); //адрес текущего пустого блока с учетом индекса предыдущего (часть с данными в блоке)
-		int currentNextIndex = currentPage->headFL->nextFreeBlockIndex; //индекс следующего блока после текущего
-		currentPage->headFL = (BlockHeader*)((char*)currentPage + (currentNextIndex * blockSize) + blockSize);// переход на указателя на следующий свободный блок по его индексу (указатель без учета индекса след эл.)
 		
+		//auto 
+	/*	while (currentPage->headFL->nextFreeBlockIndex < 0)
+		{
+
+		}*/
+			
+
+		auto currentFreeBlock = (char*)currentPage->headFL + sizeof(int);			//адрес текущего пустого блока с учетом индекса предыдущего (часть с данными в блоке)
+		int currentNextIndex = currentPage->headFL->nextFreeBlockIndex;				//индекс следующего блока после текущего
+		if (currentNextIndex > 0)
+		{
+			currentPage->headFL = (BlockHeader*)((char*)currentPage + (currentNextIndex * blockSize) + blockSize);// переход на следующий свободный блок по его индексу (указатель без учета индекса след эл.)
+		}		
+		else
+		{
+			auto newPage = AddNewPage();											//добавляем новую страницу
+			newPage->headFL = (BlockHeader*)((char*)newPage + blockSize);			//добавляем голову списка в новую страницу
+			BlockHeadersInit(newPage);												//инициализируем блоки в новой странице
+			currentPage->nextPage = newPage;										//записываем новую страницу в next для текущей страницы		
+		}
+
 		//currentPage->headFL->nextFreeBlockIndex = ++currentNextIndex;
 		//auto index2 = ((char*)currentPage->headFL - (char*)currentPage) / blockSize - 1; // расчет индекса текущего свободного элемента
 
 		return currentFreeBlock;
 	}
 
-	void SetFreeBlock(void *p)
+	void SetFreeBlock(PageHeader* curPage, void *p)	// в функцию передается текущая страница, в которой находится указатель и сам указатель
 	{
-		auto currentBlockindex = ((char*)currentPage->headFL - (char*)currentPage) / blockSize - 1; // расчет индекса текущего свободного элемента
-		currentPage->headFL = (BlockHeader*)((char*)p - sizeof(int));
-		currentPage->headFL->nextFreeBlockIndex = currentBlockindex;
+		//auto currentBlockindex = ((char*)currentPage->headFL - (char*)currentPage) / blockSize - 1; // расчет индекса текущего свободного элемента
+		//currentPage->headFL = (BlockHeader*)((char*)p - sizeof(int));
+		//currentPage->headFL->nextFreeBlockIndex = currentBlockindex;
+		auto currentBlockindex = ((char*)curPage->headFL - (char*)curPage) / blockSize - 1;	// расчет индекса текущего свободного элемента
+		curPage->headFL = (BlockHeader*)((char*)p - sizeof(int));
+		curPage->headFL->nextFreeBlockIndex = currentBlockindex;
 	}
 
-	PageHeader* AddNewPage() //здесь происходит аллок новой страницы
+	PageHeader* AddNewPage()	//выделение новой страницы
 	{
 		auto newPage = (PageHeader*)VirtualAlloc(
 			NULL,
 			PAGESIZE,
-			MEM_COMMIT,			//инициализирует страницу в ранее зарезервированную область
+			MEM_COMMIT,	//инициализирует страницу
 			PAGE_READWRITE);
 #ifdef _DEBUG
-		assert(newPage != NULL); // проверка на инициализацию страницы
+		assert(newPage != NULL);	// проверка на инициализацию страницы
 #endif // DEBUG	
 
 
@@ -95,25 +115,41 @@ public:
 
 	void InitFSA()
 	{		
-		auto newPage = AddNewPage();
-		if (currentPage == nullptr)
-		{
-			currentPage = newPage;
-			currentPage->nextPage = nullptr;
-		}			
-		else
-		{
-			currentPage->nextPage = newPage;
-		}
+		currentPage = AddNewPage();
+		//auto newPage = AddNewPage();
+		//if (currentPage == nullptr)
+		//{
+		//	currentPage = newPage;
+		//	currentPage->nextPage = nullptr;
+		//}			
+		//else
+		//{
+		//	currentPage->nextPage = newPage;
+		//}
 			
-		currentPage->headFL = (BlockHeader*)((char*)currentPage + blockSize); // смещение указателя на первый свободный блок без учета индекса след. эл.	
+		currentPage->headFL = (BlockHeader*)((char*)currentPage + blockSize);	// смещение указателя на первый свободный блок без учета индекса след. эл.	
+		
 		//currentPage->headFL->currentBlockIndex = 0; //индекс текущего блока
 		//currentPage->headFL->nextFreeBlockIndex++; //индекс следующего свободного блока
-		
-		// инициализация заголовков блоков 
-		auto temp = currentPage->headFL;
+			
+		BlockHeadersInit(currentPage);	// инициализация заголовков блоков 
+
+		//auto temp = currentPage->headFL;
+		//int i = 1;
+		//while ((char*)temp < (char*)currentPage + PAGESIZE - blockSize) 
+		//{
+		//	temp->nextFreeBlockIndex = i;
+		//	i++;
+		//	temp = (BlockHeader*)((char*)temp + blockSize);
+		//}
+		//temp->nextFreeBlockIndex = -1; // последний блок не имеет следующего
+	}
+
+	void BlockHeadersInit(PageHeader* page)
+	{
+		auto temp = page->headFL;
 		int i = 1;
-		while ((char*)temp < (char*)currentPage + PAGESIZE - blockSize) 
+		while ((char*)temp < (char*)page + PAGESIZE - blockSize)
 		{
 			temp->nextFreeBlockIndex = i;
 			i++;
@@ -228,11 +264,10 @@ public:
 		auto curPage = alloc16.currentPage;
 		while (curPage != nullptr)
 		{
-			if ((char*)p > (char*)curPage 
-				&& (char*)p < (char*)curPage + PAGESIZE)
+			if ((char*)p > (char*)curPage && (char*)p < (char*)curPage + PAGESIZE)
 				//&& ((char*)p - (char*)curPage) % alloc16.blockSize == 0) // проверка на попадание в интервал и на выравнивание по размерам ячеек
 			{
-				alloc16.SetFreeBlock(p); // помещаем ячейку в голову списка
+				alloc16.SetFreeBlock(curPage, p); // помещаем ячейку в голову списка
 				break;
 			}
 			else
--- MemoryManagementTests/test.cpp
+++ MemoryManagementTests/test.cpp
@@ -7,24 +7,41 @@ TEST(TestCaseName, TestName)
   EXPECT_TRUE(true);
 }
 
-TEST(Test, Test) 
+//TEST(Test, Test) 
+//{
+//	MemoryAllocator allocator;
+//	allocator.Init();
+//	int *p1 = (int*)allocator.Alloc(sizeof(int));
+//	*p1 = 5;
+//	int *p2 = (int*)allocator.Alloc(sizeof(int));
+//	*p2 = 7;
+//	int *p3 = (int*)allocator.Alloc(sizeof(int));
+//	*p3 = 3;
+//	EXPECT_EQ(*p1, 5);
+//	EXPECT_EQ(*p2, 7);
+//	EXPECT_EQ(*p3, 3);
+//	allocator.Free(p2);
+//	int *p4 = (int*)allocator.Alloc(sizeof(int));
+//	*p4 = 8;
+//	int *p5 = (int*)allocator.Alloc(sizeof(int));
+//	*p5 = 9;
+//	//EXPECT_EQ(*p2, 7);
+//	EXPECT_TRUE(true);
+//}
+
+TEST(Test1, Test)
 {
 	MemoryAllocator allocator;
 	allocator.Init();
-	int *p1 = (int*)allocator.Alloc(sizeof(int));
-	*p1 = 5;
-	int *p2 = (int*)allocator.Alloc(sizeof(int));
-	*p2 = 7;
-	int *p3 = (int*)allocator.Alloc(sizeof(int));
-	*p3 = 3;
-	EXPECT_EQ(*p1, 5);
-	EXPECT_EQ(*p2, 7);
-	EXPECT_EQ(*p3, 3);
-	allocator.Free(p2);
-	int *p4 = (int*)allocator.Alloc(sizeof(int));
-	*p4 = 8;
-	int *p5 = (int*)allocator.Alloc(sizeof(int));
-	*p5 = 9;
+	//int *p = (int*)allocator.Alloc(sizeof(int));
+	//*p = 1;
+	for (int i = 0; i < 255; i++)
+	{
+		int *p = (int*)allocator.Alloc(sizeof(int));
+		*p = i;
+	}
+	int *p = (int*)allocator.Alloc(sizeof(int));
+
 	//EXPECT_EQ(*p2, 7);
 	EXPECT_TRUE(true);
 }
